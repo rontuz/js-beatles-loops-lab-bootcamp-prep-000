67819e4fb07ffad7a434ec425a5e07a103a641d3
Ronald Tuzo <rontuz@icloud.com>
2018-07-17 20:27:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function theBeatlesPlay(musicians, instruments){
+  var beatlesArray = [];
+      for (let i=0; i<4; i++){
+      beatlesArray.push(musicians[i] + " plays " + instruments[i]);
+}return beatlesArray;
+}
+ 
+function johnLennonFacts(facts){
+  var n = 0;
+  var shoutedFacts = [];
+  while (n < 4) {
+    shoutedFacts.push(facts[n] +"!!!");
+  n++; 
+  }return shoutedFacts;
+  function johnLennonFacts(facts){
+  shoutedFacts = (facts[n] + "!!!");
+}
+}
+
